b5d7b7ed27cb24dfac6f00737e78abf636abde00
Azar Maharramov <azmaharramov@its.gov.az>
2025-03-25 15:41:30 +0400

    added fourzer.EntityFrameworkCore.Naming

---
+++ fourzer.EntityFrameworkCore.Naming/SnakeCaseExtension.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Text.RegularExpressions;
+
+namespace fourzer.EntityFrameworkCore.Naming
+{
+    public static class SnakeCaseExtension
+    {
+        public static void UseSnakeCase(this ModelBuilder builder)
+        {
+            foreach (var entity in builder.Model.GetEntityTypes())
+            {
+                foreach (var property in entity.GetProperties())
+                {
+                    var newName = ConvertToSnakeCase(property.Name);
+                    property.SetColumnName(newName);
+                }
+
+                var tableName = ConvertToSnakeCase(entity.GetTableName());
+                entity.SetTableName(tableName);
+            }
+        }
+
+        private static string ConvertToSnakeCase(string? name)
+        {
+            if (string.IsNullOrEmpty(name)) return string.Empty;
+
+            return Regex.Replace(name, "([a-z0-9])([A-Z])", "$1_$2").ToLowerInvariant();
+        }
+    }
+
+}
